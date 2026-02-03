12f672c9c9c79aa473fe8e8193357622512c5105
viho-kernel <vihari.reddy1802@gmail.com>
2026-02-03 10:44:31 +0530

    latest commit

--- mongodb.sh
+++ mongodb.sh
@@ -3,11 +3,11 @@
 source ./common.sh
 ROOT_ACCESS
 
-dnf install mongodb-org -y &>> $LOG_FILE
-VALIDATE $? "Installing MongoDB Server"
-
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOG_FILE
 VALIDATE $? "Copying Mongo files"
+
+dnf install mongodb-org -y &>> $LOG_FILE
+VALIDATE $? "Installing MongoDB Server"
 
 systemctl enable mongod &>> $LOG_FILE
 VALIDATE $? "Enabling MongoDB Server"
@@ -16,5 +16,9 @@ systemctl start mongod &>> $LOG_FILE
 VALIDATE $? "Enabling MongoDB Server"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing remote connections"
+
+systemctl restart mongod
+VALIDATE $? "Restarted MongoDB Service"
 
 TIME_STAMP
