eededc8e7f3fe0e8bf8c3527d170ea80b626b7ca
viho-kernel <vihari.reddy1802@gmail.com>
2026-02-03 12:18:10 +0530

    latest commit

--- redis.sh
+++ redis.sh
@@ -12,7 +12,7 @@ VALIDATE $? "Enabling Redis Version 7"
 dnf install redis -y &>> $LOG_FILE
 VALIDATE $? "Installing Redis"
 
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/c\/protected-mode no' /etc/redis/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/c/protected-mode no' /etc/redis/redis.conf
 VALIDATE $? "Enabling Remote connection"
 
 systemctl enable redis &>> $LOG_FILE
