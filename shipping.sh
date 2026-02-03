d95e1c4cbac96f3bd6132f21469eefc8e6e69ec7
viho-kernel <vihari.reddy1802@gmail.com>
2026-02-03 14:24:34 +0530

    latest commit

--- shipping.sh
+++ shipping.sh
@@ -5,7 +5,7 @@ source ./common.sh
 JAVA_SETUP
 ID_USER
 DAEMON
-MYSQL_SETUP
+
 
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip &>> $LOG_FILE
 VALIDATE $? "Downloading Shipping Code"
@@ -20,6 +20,8 @@ VALIDATE $? "Builing maven file"
 
 mv target/shipping-1.0.jar shipping.jar &>> $LOG_FILE
 VALIDATE $? "Moving the Jar files"
+
+MYSQL_SETUP
 
 systemctl restart shipping
 
