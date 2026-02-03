7095eb32c3fb458d44a2914dd6160c8a572595dc
viho-kernel <vihari.reddy1802@gmail.com>
2026-02-03 14:33:02 +0530

    latest commit

--- shipping.sh
+++ shipping.sh
@@ -3,27 +3,51 @@
 source ./common.sh
 NAME=shipping
 ROOT_ACCESS
-JAVA_SETUP
+
+dnf install maven -y &>> $LOG_FILE
+VALIDATE $? "Installing Maven (Java)"
+
 ID_USER
-DAEMON
 
+mkdir -p /app $>> $LOG_FILE
+VALIDATE $? "Creating /app directory"
 
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip &>> $LOG_FILE
-VALIDATE $? "Downloading Shipping Code"
+cd /app
+VALIDATE $? "Moving to /app directory"
 
-cd /app 
+curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip  &>> $LOG_FILE
+VALIDATE $? "Downloading Shipping Artifact"
 
-unzip /tmp/shipping.zip
-VALIDATE $? "Unzipping the files"
+unzip /tmp/shipping.zip &>> $LOG_FILE
+VALIDATE $? "Unzipping the shipping"
 
 mvn clean package &>> $LOG_FILE
-VALIDATE $? "Builing maven file"
+VALIDATE $? "Building Shipping Application with Maven"
 
 mv target/shipping-1.0.jar shipping.jar &>> $LOG_FILE
-VALIDATE $? "Moving the Jar files"
+VALIDATE $? "Renaming shipping jar"
 
-MYSQL_SETUP
+cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service &>> $LOG_FILE
+VALIDATE $? "Copying Shipping Service file"
+
+systemctl daemon-reload
+systemctl enable shipping &>>$LOG_FILE
+systemctl start shipping
+VALIDATE $? "Starting Shipping Service"
+
+dnf install mysql -y &>> $LOG_FILE
+VALIDATE $? "Installing MySQL Client"
+
+mysql -h ${MYSQL_HOST} -uroot -pRoboShop@1 < /app/db/schema.sql
+VALIDATE $? "Loading schema.sql into MySQL"
+
+mysql -h ${MYSQL_HOST} -uroot -pRoboShop@1 < /app/db/app-user.sql 
+VALIDATE $? "Loading app-user.sql into MySQL"
+
+mysql -h ${MYSQL_HOST} -uroot -pRoboShop@1 < /app/db/master-data.sql
+VALIDATE $? "Loading master-data.sql into MySQL"
 
 systemctl restart shipping
+VALIDATE $? "Restarting Shipping Service"
 
 TIME_STAMP
