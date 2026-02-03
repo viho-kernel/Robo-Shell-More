327b28c18b2e530b78e7115b314e214b2acf543f
viho-kernel <vihari.reddy1802@gmail.com>
2026-02-03 14:06:25 +0530

    latest commit

--- mysql.sh
+++ mysql.sh
@@ -9,7 +9,7 @@ VALIDATE $? "Installing Mysql server"
 systemctl enable mysqld &>> $LOG_FILE
 VALIDATE $? "Installing enabling server"
 
-systemctl start mysqld $>> $LOG_FILE
+systemctl start mysqld
 VALIDATE $? "Starting MySQL Server"
 
 mysql_secure_installation --set-root-pass RoboShop@1
