e0bc56b7446b0bf371b27f915908273f9e7202dd
viho-kernel <vihari.reddy1802@gmail.com>
2026-02-03 11:19:45 +0530

    latest commit

---
+++ catalogue.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+NAME=catalogue
+source ./common.sh
+ROOT_ACCESS
+NODE_JS
+ID_USER
+APP_SETUP
+DAEMON
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOG_FILE
+VALIDATE $? "Copying the MongoDB File"
+
+dnf install mongodb-mongosh -y &>> $LOG_FILE
+
+if [ $(mongosh --host $MONGODB_HOST --quiet --eval 'db.getMongo().getDBNames().indexOf("catalogue")' ) -le 0 ]; then
+    VALIDATE $? "Loading products"
+else
+    mongosh --host $MONGODB_HOST </app/db/master-data.js
+    echo -e " $(date "+%Y-%m-%d %H:%M:%S") | Products already loaded. "
+fi
+
+
--- common.sh
+++ common.sh
@@ -5,7 +5,6 @@ LOG_FOLDER="/var/log/Shell-Roboshop-logs"
 LOG_FILE="$LOG_FOLDER/$0.log"
 SCRIPT_DIR=$(pwd)
 
-
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
@@ -35,8 +34,54 @@ VALIDATE() {
     fi
 }
 
+NODE_JS(){
+dnf module disable nodejs -y &>> $LOG_FILE
+VALIDATE $? "Disabling NodeJS"
 
+dnf module enable nodejs:20 -y &>> $LOG_FILE
+VALIDATE $? "Enabling NodeJS"
 
+dnf install nodejs -y &>> $LOG_FILE
+VALIDATE $? "Installing NodeJS"
+}
+
+ID_USER(){
+id roboshop &>> $LOG_FILE
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATE $? "Creating User"
+else
+   echo -e "Roboshop user already exist ... $Y Skipping $N"
+fi
+}
+
+APP_SETUP(){
+    mkdir -p /app &>> $LOG_FILE
+    VALIDATE $? "Creating APP Directory!"
+
+    curl -o /tmp/$NAME.zip https://roboshop-artifacts.s3.amazonaws.com/$NAME-v3.zip &>> $LOG_FILE
+    VALIDATE $? "Downloading Application Code"
+    cd /app 
+    VALIDATE $? "Moving to app Directory"
+    rm -rf /app* &>> $LOG_FILE
+    VALIDATE $? "Removing Exisiting Code"
+    unzip /tmp/$NAME.zip
+    VALIDATE $? "Unzipping the Code"
+    npm install
+    VALIDATE $? "Installing Dependencies"
+}
+
+DAEMON(){
+    cp $SCRIPT_DIR/$NAME.service /etc/systemd/system/$NAME.service &>> $LOG_FILE
+VALIDATE $? "Copying the service file"
+
+    systemctl daemon-reload &>> $LOG_FILE
+    VALIDATE $? "Reload the Daemon"
+    systemctl enable $NAME  &>> $LOG_FILE
+    VALIDATE $? "Enable $NAME Service"
+    systemctl start $NAME &>> $LOG_FILE
+    VALIDATE $? "Start $NAME Seervice"
+}
 TIME_STAMP(){
     END_TIME=$(date +%s)
     echo "Script ended executing at: $(date '+%Y-%m-%d %H:%M:%S') " | tee -a $LOG_FILE
