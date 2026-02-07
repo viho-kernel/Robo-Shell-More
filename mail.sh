142eabdb6dafd1f92bf5455a2adf30038e163066
viho-kernel <vihari.reddy1802@gmail.com>
2026-02-07 16:12:23 +0530

    latest commit

--- mail.sh
+++ mail.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
 
+TO_ADDRESS=$1
+SUBJECT=$2
+ORDER_ID=$3
+PROBE_COST=$4
+CYBORG_COST=$5
+SHIPPING_COST=$6
+TAX_COST=$7
+TOTAL_AMOUNT=$8
 
-
+EMAIL_BODY=$(sed -e "s|ORDER_ID|$ORDER_ID|g" \
+                  -e "s|PROBE_COST|$PROBE_COST|g" \
+                  -e "s|CYBORG_COST|$CYBORG_COST|g" \
+                   -e "s|SHIPPING_COST|$SHIPPING_COST|g" \
+                   -e "s|TAX_COST|$TAX_COST|g" \
+                  -e "s|TOTAL_AMOUNT|$TOTAL_AMOUNT|g" mail.html)
 
 
 {
